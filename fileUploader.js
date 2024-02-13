a99f52cc868417215b0d29cfcf01f00657a36642
Sergei Volkov <volkov-sv@Sergeis-MacBook-Pro.local>
2024-02-13 14:09:11 +0100

    Comments removed

--- fileUploader.js
+++ fileUploader.js
@@ -1,6 +1,8 @@
 const forma = document.getElementById("wf-form-QR-Robot-feedback");
+const errorMessage = document.querySelector(".w-form-fail");
+const errorMessageDiv = document.querySelector(".error-message");
+const submitButtonText =  document.querySelector(".btn-text-ovrl-qr-pf");
 
-// Необходимо инициировать срабатывания события для подписывания файлов на стороне вебфлоу, отправка запросов
 const changeEvent = new Event("change", { bubbles: true });
 
 const targetUrl =
@@ -20,9 +22,7 @@ function processSelectedFiles(selectedFiles, fileInputs) {
 
   selectedFiles.forEach((file, index) => {
     dtt.items.add(file);
-    console.log(dtt.files);
     fileInputs[index].files = dtt.files;
-    console.log(fileInputs[index].files);
     dtt = new DataTransfer();
     fileInputs[index].dispatchEvent(new Event('change'));
   });
@@ -43,31 +43,20 @@ const xhrInterceptor = (numRequests, successCallback, errorCallback) => {
         let successfulRequests = 0;
         let totalRequests = 0;
 
-        // const timeoutId = setTimeout(() => {
-        //     console.log(
-        //         `Таймаут. Получено успешных запросов: ${successfulRequests}, всего запросов: ${totalRequests}`
-        //     );
-        //     reject(false);
-        //     XMLHttpRequest.prototype.open = open;
-        // }, 10000);
-
         XMLHttpRequest.prototype.open = function (method, url) {
             const self = this;
             
             const loadHandler = function () {
-    console.log(`GET запрос по адресу ${url}, статус: ${self.status}`);
 
     if (self.status === 204) {
         successfulRequests++;
 
         if (successfulRequests === numRequests) {
-            console.log(`Запросы исчерпаны. Получено успешных запросов: ${successfulRequests}, всего запросов: ${totalRequests}`);
             resolve(successCallback());
             XMLHttpRequest.prototype.open = open;
             self.removeEventListener("load", loadHandler);
         }
     } else {
-        console.log(`Запрос с ошибкой. Получено успешных запросов: ${successfulRequests}, всего запросов: ${totalRequests}. GET запрос по адресу ${url}, статус: ${self.status}.`);
         reject(errorCallback());
         XMLHttpRequest.prototype.open = open;
         self.removeEventListener("load", loadHandler);
@@ -85,19 +74,16 @@ const xhrInterceptor = (numRequests, successCallback, errorCallback) => {
 };
 
 function successCallbackFunction() {
-    console.log("Success");
     forma.requestSubmit();
 }; 
 
 function errorCallbackFunction() {
-    console.log("Fault");
-    document.querySelector(".w-form-fail").style.display = "block";
+    errorMessage.style.display = "block";
 };
 
 const submitHandler = function (event) {
   if (forma.selectedFiles.length > 0 && forma.selectedFiles.length < 5) {
     flag = true;
-    console.log("FIRE");
     document.dispatchEvent(new Event("flag"));
     $("#wf-form-QR-Robot-feedback").off("submit", submitHandler);
     return false;
@@ -110,8 +96,7 @@ var flag = false;
 document.addEventListener("flag", function () {
   if (flag) {
     flag = false;
-    console.log("GOTCHA");
-    document.querySelector(".btn-text-ovrl-qr-pf").innerText = "Uploading...";
+    submitButtonText.innerText = "Uploading...";
     processSelectedFiles(forma.selectedFiles, fileInputs);
   }
 });
@@ -120,25 +105,30 @@ var clickHandler;
 var timer;
 
 function clickHandlerOnce() {
-  console.log("Клик произошел, удаляем слушатель");
-  document.querySelector(".w-form-fail").style.display = "none";
+  errorMessageDiv.classList.remove('visible-fade');
+  setTimeout(function() {
+      errorMessage.style.display = "none";
+  }, 500);
   document.removeEventListener("click", clickHandlerOnce);
   clearTimeout(timer);
 }
 
 function handleMutation(mutationsList, observer) {
   mutationsList.forEach(function(mutation) {
-    if (mutation.attributeName === 'style' && document.querySelector(".w-form-fail").style.display === 'block') {
-      console.log("Произошло изменение атрибута style");
+    if (mutation.attributeName === 'style' && errorMessage.style.display === 'block') {
+      errorMessageDiv.classList.add('visible-fade');
 
       if (clickHandler) {
           document.removeEventListener("click", clickHandler);
       }
       clickHandler = document.addEventListener("click", clickHandlerOnce);
 
+      clearTimeout(timer);
       timer = setTimeout(function() {
-        console.log("Таймер сработал");
-        document.querySelector(".w-form-fail").style.display = "none";
+        errorMessageDiv.classList.remove('visible-fade');
+        setTimeout(function() {
+            errorMessage.style.display = "none";
+        }, 500);
         document.removeEventListener("click", clickHandlerOnce);
       }, 3000);
     }
@@ -147,6 +137,6 @@ function handleMutation(mutationsList, observer) {
 
 var observer = new MutationObserver(handleMutation);
 
-var targetNode = document.querySelector(".w-form-fail");
+var targetNode = errorMessage;
 var config = { attributes: true, attributeFilter: ['style'] };
 observer.observe(targetNode, config);
